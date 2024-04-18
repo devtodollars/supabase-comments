61a7fb07f972eb0caf2cf0e7d87b66685770cbaa
Matthew Wong <matthewwong525@gmail.com>
2024-04-18 13:02:10 -0400

    adjust replyeditor

--- src/components/ReplyEditor.tsx
+++ src/components/ReplyEditor.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 import useAddComment from "@/hooks/useAddComment";
 import useAuth from "@/hooks/useAuth";
 
@@ -15,11 +18,11 @@ const ReplyEditor: React.FC<ReplyEditorProps> = ({
   onClose,
   onLogout,
 }) => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
   const [replyText, setReplyText] = useState<string>("");
   const addCommentMutation = useAddComment();
 
-  const handleReplyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleReplyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setReplyText(e.target.value);
   };
 
@@ -40,16 +43,34 @@ const ReplyEditor: React.FC<ReplyEditorProps> = ({
   };
 
   return (
-    <div>
-      <button onClick={handleLogout}>Logout</button>
-      <input
-        type="text"
+    <div className="flex flex-col">
+      <div className="flex items-center">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="User avatar" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+      <Textarea
+        className="flex-1 mt-2"
         value={replyText}
         onChange={handleReplyChange}
-        placeholder="Write a reply..."
+        placeholder="Write a comment..."
       />
-      <button onClick={submitReply}>Submit Reply</button>
-      {onClose && <button onClick={onClose}>Cancel</button>}
+      <div className="flex justify-between items-center mt-2">
+        {user && (
+          <Button variant="link" onClick={handleLogout} className="ml-1">
+            Logout
+          </Button>
+        )}
+        <div className="flex items-center">
+          {onClose && (
+            <Button className="mr-2" onClick={onClose}>
+              Cancel
+            </Button>
+          )}
+          <Button onClick={submitReply}>Comment</Button>
+        </div>
+      </div>
     </div>
   );
 };
