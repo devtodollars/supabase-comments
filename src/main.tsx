c0ad88074e08359b2b4393bee43b6b8fc1e75afc
Matthew Wong <matthewwong525@gmail.com>
2024-04-19 11:00:48 -0400

    fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import CommentsProvider from "@/components/CommentsProvider";
 import ReplyEditor from "@/components/ReplyEditor";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeMinimal } from "@supabase/auth-ui-shared";
 
 const SUPABASE_URL = "http://localhost:54321";
 const SUPABASE_ANON_KEY =
@@ -12,13 +13,12 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CommentsProvider supabaseClient={supabase} authComponent={
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeMinimal }}
-      />
-    }>
-
+    <CommentsProvider
+      supabaseClient={supabase}
+      authComponent={
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeMinimal }} />
+      }
+    >
       <div className="flex flex-col items-center justify-start min-h-screen">
         <div className="w-full max-w-2xl">
           <ReplyEditor topic="Single Topic" parentId={null} />
