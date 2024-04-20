a459f2299585c3eafb5521b41618fd9bd22d44e2
Matthew Wong <matthewwong525@gmail.com>
2024-04-20 19:29:15 -0400

    make stuff nicer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Main library exports - these are packaged in your distributable
-import "@/global.css";
 export { default as CommentsSection } from "@/components/CommentsSection";
 export type { CommentsSectionProps } from "@/components/CommentsSection";
+import "@/global.css";
