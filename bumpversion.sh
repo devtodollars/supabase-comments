3a3dcb4103821da4550688ffc05cdd49c2d45355
Matthew Wong <matthewwong525@gmail.com>
2024-04-20 23:48:35 -0400

    add pipelines and bumpversion script

---
+++ bumpversion.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+VERSION=""
+
+# Function to display usage
+usage() {
+  echo "Usage: $0 --version <version-number>"
+  exit 1
+}
+
+# Manual parsing of arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --version) VERSION="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
+
+# Check for mandatory version option
+if [ -z "$VERSION" ]; then
+    echo "--version argument is required."
+    usage
+fi
+
+# Locate the project root directory
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+# Path to the manifest.json file
+PACKAGEJSONPATH="$PROJECT_ROOT/package.json"
+
+# Check if manifest.json exists
+if [ ! -f "$PACKAGEJSONPATH" ]; then
+    echo "manifest.json does not exist at $PACKAGEJSONPATH"
+    exit 1
+fi
+
+# Update the version in manifest.json
+sed -i '' 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$VERSION\",|" $PACKAGEJSONPATH
+
+# Commit the changes (optional)
+git add "$PACKAGEJSONPATH"
+git commit -m "Update version to $VERSION" $PACKAGEJSONPATH
+
+# git push changes
+git push
+git tag $VERSION
+git push origin refs/tags/$VERSION
+
+echo "Version updated to $VERSION in manifest.json and ran build command"
