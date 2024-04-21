03986ddff81131325b4adb948dda0ea1e8079673
Matthew Wong <matthewwong525@gmail.com>
2024-04-20 23:35:22 -0400

    add readme and contributing guide

--- supabase/migrations/01_init.sql
+++ supabase/migrations/01_init.sql
@@ -36,3 +36,39 @@ SELECT
     COALESCE(users.raw_user_meta_data ->> 'name', users.raw_user_meta_data ->> 'full_name', users.raw_user_meta_data ->> 'user_name') AS name,
     COALESCE(users.raw_user_meta_data ->> 'avatar_url', users.raw_user_meta_data ->> 'avatar') AS avatar
 FROM auth.users;
+
+-- Enable Row-Level Security on the table
+ALTER TABLE public.sc_comments ENABLE ROW LEVEL SECURITY;
+
+-- Create policy to allow all users to select (view) comments
+CREATE POLICY "Enable access to all users"
+    ON public.sc_comments
+    AS PERMISSIVE
+    FOR SELECT
+    TO public
+    USING (true);
+
+-- Create policy to allow users to delete their own comments
+CREATE POLICY "Enable delete for users based on user_id"
+    ON public.sc_comments
+    AS PERMISSIVE
+    FOR DELETE
+    TO authenticated
+    USING ((auth.uid() = user_id));
+
+-- Create policy to allow authenticated users to insert new comments
+CREATE POLICY "Enable insert for authenticated users only"
+    ON public.sc_comments
+    AS PERMISSIVE
+    FOR INSERT
+    TO authenticated
+    WITH CHECK ((auth.role() = 'authenticated') AND (user_id = auth.uid()));
+
+-- Create policy to allow users to update their own comments
+CREATE POLICY "Enable update for users based on user_id"
+    ON public.sc_comments
+    AS PERMISSIVE
+    FOR UPDATE
+    TO authenticated
+    USING ((auth.uid() = user_id))
+    WITH CHECK (auth.uid() = user_id);
